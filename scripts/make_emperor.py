d1ec4aa5a97f1cc8d71813acc9a7885426382192
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2013-03-31 12:44:08 -0600

    Add script_usage entries for jackknifing

--- scripts/make_emperor.py
+++ scripts/make_emperor.py
@@ -45,7 +45,18 @@ script_info['script_usage'] = [("Plot PCoA data","Visualize the a PCoA file "
     ("PCoA plot with an explicit axis", "Create a PCoA plot with an axis of "
     "the plot representing the 'DOB' of the samples. This option is useful when"
     " presenting a gradient from your metadata e. g. 'Time' or 'pH': ", "%prog "
-    "-i unweighted_unifrac_pc.txt -m Fasting_Map.txt -a DOB -o pcoa_dob")]
+    "-i unweighted_unifrac_pc.txt -m Fasting_Map.txt -a DOB -o pcoa_dob"),
+    ("Jackknifed principal coordinates analysis plot", "Create a jackknifed "
+    "PCoA plot (with confidence intervals for each sample) passing as the input"
+    " a directory of coordinates files (where each file corresponds to a "
+    "different OTU table) and use the standard deviation method to compute the "
+    "dimensions of the ellipsoids surrounding each sample: ", "%prog -i "
+    "unweighted_unifrac_pc -m Fasting_Map.txt -o jackknifed_pcoa -s sdev"),
+    ("Jackknifed PCoA plot with a master coordinates file", "Passing a master "
+    "coordinates file (--master_pcoa) will display the ellipsoids centered by "
+    "the samples in this file: ", "%prog -i unweighted_unifrac_pc -s "
+    "unweighted_unifrac_pc/unweighted_unifrac_pc_120_7.txt -m Fasting_Map.txt "
+    "-o jackknifed_with_master")]
 script_info['output_description']= "This script creates an output directory "+\
     "with an HTML formated file named 'emperor.html' and a complementary "+\
     "folder named 'emperor_required_resources'. Opening emperor.html with "+\
