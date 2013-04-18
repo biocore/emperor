62598678bb69d9a8e43a2d450323f9a2ed3639dc
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2013-04-17 18:10:23 -0600

    Add controller for biplot processing & test suite
    This controller function will let you process the raw data i. e. otu
    table and coordinates file and will let you retrieve the scores and N
    most prevalent taxons.

--- emperor/biplots.py
+++ emperor/biplots.py
@@ -13,9 +13,10 @@ __status__ = "Development"
 
 from numpy import argsort
 
-from qiime.biplots import get_taxa_prevalence, get_taxa_coords
+from qiime.biplots import (get_taxa_prevalence, get_taxa_coords,
+    make_biplot_scores_output)
 
-def extract_taxa_data(otu_coords, otu_table, lineages, prevalence, N):
+def extract_taxa_data(otu_coords, otu_table, lineages, prevalence, N=0):
     """Extrac the N most prevalent elements according to a prevalence vector
 
     Inputs:
@@ -24,7 +25,8 @@ def extract_taxa_data(otu_coords, otu_table, lineages, prevalence, N):
     lineages: taxonomic assignments for each row in the otu_table
     prevalence: vector with prevalnce from 0 to 1 for each row of the otu_table
     as returned from qiime.biplots.get_taxa_prevalence
-    N: number of most prevalent elements to retain
+    N: number of most prevalent elements to retain, if zero is passed, will
+    reatain all available
 
     Outputs:
     otu_coords: N most prevalent coords
@@ -51,3 +53,43 @@ def extract_taxa_data(otu_coords, otu_table, lineages, prevalence, N):
     out_prevalence = prevalence[indices]
 
     return out_otu_coords, out_otu_table, out_otu_lineages, out_prevalence
+
+def preprocess_otu_table(otu_sample_ids, otu_table, lineages,
+                        coords_data, N=0, sample_ids=None):
+    """Preprocess the OTU table to to generate the required data for the biplots
+
+    Input:
+    otu_sample_ids: sample identifiers for the otu_table
+    otu_table: contingency table
+    lineages: taxonomic assignments for the OTUs in the otu_table
+    coords_data: principal coordinates data where the taxa will be mapped
+    N: number of most prevalent taxa to keep, by default will use all
+    sample_ids: sample identifiers to keep from the OTU table, if None is passed
+    then all sample ids will be used
+
+    Output:
+    otu_coords: coordinates representing the N most prevalent taxa in otu_table
+    otu_table: N most prevalent OTUs from the input otu_table
+    otu_lineages: taxonomic assignments corresponding to the N most prevalent
+    OTUs
+    otu_prevalence: vector with the prevalence scores of the N highest values
+    lines: coords where the N most prevalent taxa will be positioned in the
+    biplot
+    """
+
+    # keep only the sample ids as suggested by the input argument
+    if sample_ids:
+        indices = [otu_sample_ids.index(sample_id) for sample_id in sample_ids]
+        otu_table = otu_table[:,indices]
+
+    # retrieve the prevalence and the coords prior the filtering
+    prevalence = get_taxa_prevalence(otu_table)
+    bi_plot_coords = get_taxa_coords(otu_table, coords_data)
+
+    o_otu_coords, o_otu_table, o_otu_lineages, o_prevalence =\
+        extract_taxa_data(coords_data, otu_table, lineages, prevalence, N)
+
+    lines = '\n'.join(make_biplot_scores_output({'coord': o_otu_coords,
+        'lineages': o_otu_lineages}))
+
+    return o_otu_coords, o_otu_table, o_otu_lineages, o_prevalence, lines
