aff4c33d9115c0c28f2d5ef1912b87c1f743cbf1
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2013-05-14 17:44:41 -0600

    Change __version__ and __status__ strings
    0.0.0 -> 0.9.0
    Development -> Release

--- tests/test_biplots.py
+++ tests/test_biplots.py
@@ -6,10 +6,10 @@ __author__ = "Yoshiki Vazquez Baeza"
 __copyright__ = "Copyright 2013, The Emperor Project"
 __credits__ = ["Yoshiki Vazquez Baeza"]
 __license__ = "GPL"
-__version__ = "0.0.0-dev"
+__version__ = "0.9.0"
 __maintainer__ = "Yoshiki Vazquez Baeza"
 __email__ = "yoshiki89@gmail.com"
-__status__ = "Development"
+__status__ = "Release"
 
 from numpy import array
 from qiime.parse import parse_classic_otu_table
