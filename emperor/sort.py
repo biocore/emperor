c53c56fbdb2cb6a666dc2d8de06473d0fde69739
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2013-05-14 20:39:01 -0600

    Change __version__ and __status__ strings
    Moved from 0.9.0 to 0.9.0-dev

--- emperor/sort.py
+++ emperor/sort.py
@@ -6,10 +6,10 @@ __author__ = "Yoshiki Vazquez Baeza"
 __copyright__ = "Copyright 2013, The Emperor Project"
 __credits__ = ["Yoshiki Vazquez Baeza"]
 __license__ = "GPL"
-__version__ = "0.9.0"
+__version__ = "0.9.0-dev"
 __maintainer__ = "Yoshiki Vazquez Baeza"
 __email__ = "yoshiki89@gmail.com"
-__status__ = "Release"
+__status__ = "Development"
 
 
 from numpy import zeros
