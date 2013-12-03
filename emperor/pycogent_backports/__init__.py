7ca006944edb175eca40a0f20eb02d9157a6a811
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2013-12-02 22:39:55 -0700

    Add pycogent_backports and tests
    The main changes that took place in this code-base are  the usage of
    Python's native unittest and numpy assert_almost_equal.

---
+++ emperor/pycogent_backports/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+__author__ = "Emperor Development Team"
+__copyright__ = "Copyright 2013, The Emperor Project"
+__credits__ = ["Yoshiki Vazquez Baeza"] 
+__license__ = "BSD"
+__version__ = "0.9.2-dev"
+__maintainer__ = "Yoshiki Vazquez Baeza"
+__email__ = "yoshiki89@gmail.com"
+__status__ = "Development"
+
+__all__ = ['procrustes']
