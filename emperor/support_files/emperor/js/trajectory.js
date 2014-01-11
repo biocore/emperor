a2074062ae76548cc636e32f9ca1bc04a89d2469
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2014-01-10 17:31:24 -0700

    Change trajectory.js to add docstrings

--- emperor/support_files/emperor/js/trajectory.js
+++ emperor/support_files/emperor/js/trajectory.js
@@ -1,21 +1,57 @@
 /**
  *
  * @author Yoshiki Vazquez Baeza
- * @copyright Copyright 2013, Emperor
+ * @copyright Copyright 2013, The Emperor Project
  * @credits Yoshiki Vazquez Baeza
  * @license BSD
- * @version 0.9.2-dev
+ * @version 0.9.3-dev
  * @maintainer Yoshiki Vazquez Baeza
  * @email yoshiki89@gmail.com
  * @status Development
  *
  */
 
+/**
+ *
+ * @name TrajectoryOfSamples
+ *
+ * @class Represents an ordered set of samples where each sample is indexed by
+ * the sample identifier and the order is as given by their position along the
+ * gradient.
+ *
+ * @property {Array} [sampleNames=Array()] array of sample identifiers.
+ * @property {Array} [gradientPoints=Array()] array of values where each of the
+ * samples exist in.
+ * @property {float} [minimumDelta=float] minimum differential between samples
+ * in the trajectory; the value is computed using the gradientPoints array.
+ * @property {float} [suppliedN=float] minimum number of frames a distance will
+ * have in the gradient.
+ * @property {Array} [interpolatedCoordinates=Array()] array of objects with the
+ * corresponding interpolated x, y and z values. The interpolation operation
+ * takes place between subsequent samples.
+ *
+ */
 
 /**
  *
+ * @name TrajectoryOfSamples
  *
- */
+ * @class Represents an ordered set of samples and their position in PCoA space.
+ *
+ * @param {sampleNames} an Array of strings where each string is a sample
+ * identifier.
+ * @param {gradientPoints} an Array of floating point values where each value
+ * corresponds to the position of the samples in the gradient.
+ * @param {coordinates} an Array of objects with x, y and z properties where
+ * each corresponds to the position of a sample in PCoA space.
+ * @param {minimumDelta} minimum differential between the ordered gradientPoints
+ * this value must be non-zero. Note that this value should be computed taking
+ * into account all the other trajectories that will be animated together,
+ * usually by an AnimationDirector object.
+ * @param {suppliedN} a parameter that will determine how many points should
+ * should be found in the the trajectory.
+ *
+ **/
 function TrajectoryOfSamples(sampleNames, gradientPoints, coordinates,
 							 minimumDelta, suppliedN){
 	this.sampleNames = sampleNames;
@@ -34,7 +70,8 @@ function TrajectoryOfSamples(sampleNames, gradientPoints, coordinates,
 	this.suppliedN = suppliedN !== undefined ? suppliedN : 5;
 
 	if (this.coordinates.length != this.gradientPoints.length) {
-		console.log("That's a bold move Cotton, let's see if it pays off");
+		throw new Error("The number of coordinate points and gradient points "+
+						"is different, make sure these values are consistent."); 
 	}
 
 	// initialize as an empty array but fill it up upon request
@@ -45,6 +82,8 @@ function TrajectoryOfSamples(sampleNames, gradientPoints, coordinates,
 
 /**
  *
+ * Helper method to iterate over all the coordinates and generate interpolated
+ * arrays.
  *
  */
 TrajectoryOfSamples.prototype._generateInterpolatedCoordinates = function(){
@@ -78,6 +117,18 @@ TrajectoryOfSamples.prototype._generateInterpolatedCoordinates = function(){
 
 /**
  *
+ * Helper function to calculate the number of points that there should be for a
+ * differential.
+ *
+ * @param {delta} float value for which to determine the required number of
+ * points.
+ * @param {suppliedN} int value, usually a constant defined by the
+ * TrajectoryOfSamples class.
+ * @param {minimumDelta} float value to represent the minimum differential found
+ * in a set of trajectories.
+ *
+ * @return an integer representing the number of suggested frames for the
+ * differential
  *
  */
 function calculateNumberOfPointsForDelta(delta, suppliedN, minimumDelta) {
@@ -85,10 +136,23 @@ function calculateNumberOfPointsForDelta(delta, suppliedN, minimumDelta) {
 }
 
 /**
+ * 
+ * Function to interpolate a certain number of steps between two three
+ * dimensional points.
  *
- *
- * very heavily based on the code found on:
+ * This code is based on the function found in:
  *     http://snipplr.com/view.php?codeview&id=47206
+ *
+ * @param x_1 float initial value of a position in the first dimension
+ * @param y_1 float initial value of a position in the second dimension
+ * @param z_1 float initial value of a position in the third dimension
+ * @param x_2 float final value of a position in the first dimension
+ * @param y_2 float final value of a position in the second dimension
+ * @param z_2 float final value of a position in the third dimension
+ * @param steps integer number of steps that we want the interpolation to run
+ *
+ * @return Array with a objects that have an x, y and z attributes
+ *
  */
 function linearInterpolation( x_1, y_1, z_1, x_2, y_2, z_2, steps){
 	var xAbs = Math.abs(x_1-x_2);
@@ -122,9 +186,20 @@ function linearInterpolation( x_1, y_1, z_1, x_2, y_2, z_2, steps){
 
 /**
  *
+ * Function to compute the distance between two three dimensional points.
  *
- * very heavily based on the code found on: 
+ * This code is based on the function found in:
  *     http://snipplr.com/view.php?codeview&id=47207
+ *
+ * @param x_1 float initial value of a position in the first dimension
+ * @param y_1 float initial value of a position in the second dimension
+ * @param z_1 float initial value of a position in the third dimension
+ * @param x_2 float final value of a position in the first dimension
+ * @param y_2 float final value of a position in the second dimension
+ * @param z_2 float final value of a position in the third dimension
+ *
+ * @return floating point value of the distance between the two points
+ *
  */
 function distanceBetweenPoints( x_1, y_1, z_1, x_2, y_2, z_2){
 	var xs = 0;
@@ -141,6 +216,29 @@ function distanceBetweenPoints( x_1, y_1, z_1, x_2, y_2, z_2){
 
 /**
  *
+ * Helper data wrangling function, takes as inputs a mapping file and the
+ * coordinates to synthesize the information into an array. Mainly used by the
+ * AnimationDirector object.
+ * 
+ * @param {mappingFileHeaders} an Array of strings containing metadata mapping
+ * file headers (required).
+ * @param {mappingFileData} an Array where the indices are sample identifiers
+ * and each of the contained elements is an Array of strings where the first
+ * element corresponds to the first data for the first column in the mapping
+ * file (mappingFileHeaders) (required).
+ * @param {coordinatesData} an Array of Objects where the indices are the sample
+ * identifiers and each of the objects has the following properties: x, y, z,
+ * name, color, P1, P2, P3, ... PN where N is the number of dimensions in this
+ * dataset (required).
+ * @param {trajectoryCategory} a string with the name of the mapping file header
+ * where the data that groups the samples is contained, this will usually be
+ * BODY_SITE, HOST_SUBJECT_ID, etc. (required).
+ * @param {gradientCategory} a string with the name of the mapping file header
+ * where the data that spreads the samples over a gradient is contained, usually
+ * time or days_since_epoch. Note that this should be an all numeric category
+ * (required). 
+ *
+ * @return an array with the contained data indexed by the sample identifiers.
  *
  */
 function getSampleNamesAndDataForSortedTrajectories(mappingFileHeaders,
@@ -194,6 +292,19 @@ function getSampleNamesAndDataForSortedTrajectories(mappingFileHeaders,
 
 /**
  *
+ * Function to calculate the minimum delta from an array of wrangled data by
+ * getSampleNamesAndDataForSortedTrajectories.
+ *
+ * @param sampleData is an Array as computed from mapping file data and
+ * coordinates by getSampleNamesAndDataForSortedTrajectories.
+ *
+ * @return float value with the minimum difference between two samples across
+ * the defined gradient.
+ * 
+ * This function will raise an Error in case the input data is undefined.
+ * This function will not take into account as a minimum delta zero values i. e.
+ * the differential between two samples that lie at the same position in the
+ * gradient.
  *
  */
 function getMinimumDelta(sampleData){
