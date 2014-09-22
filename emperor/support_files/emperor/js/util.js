5fc09e2c340116d489dda4abb19c649b3e95e51a
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2014-09-22 15:57:18 -0600

    ENH: Use colorbrewer for discrete coloring

--- emperor/support_files/emperor/js/util.js
+++ emperor/support_files/emperor/js/util.js
@@ -12,32 +12,34 @@
  */
 
 
-// taken from the qiime/colors.py module; a total of 24 colors
-var k_QIIME_COLORS = [
-"0xFF0000", // red1
-"0x0000FF", // blue1
-"0xF27304", // orange1
-"0x008000", // green
-"0x91278D", // purple1
-"0xFFFF00", // yellow1
-"0x7CECF4", // cyan1
-"0xF49AC2", // pink1
-"0x5DA09E", // teal1
-"0x6B440B", // brown1
-"0x808080", // gray1
-"0xF79679", // red2
-"0x7DA9D8", // blue2
-"0xFCC688", // orange2
-"0x80C99B", // green2
-"0xA287BF", // purple2
-"0xFFF899", // yellow2
-"0xC49C6B", // brown2
-"0xC0C0C0", // gray2
-"0xED008A", // red3
-"0x00B6FF", // blue3
-"0xA54700", // orange3
-"0x808000", // green3
-"0x008080"] // teal3
+// http://colorbrewer2.org > qualitative > Number of Data Classes = 12
+// colorbrewer will provide you with two lists of colors, those have been
+// added here
+var k_COLORBREWER_COLORS = [
+"0x8dd3c7", // first list starts here
+"0xffffb3",
+"0xbebada",
+"0xfb8072",
+"0x80b1d3",
+"0xfdb462",
+"0xb3de69",
+"0xfccde5",
+"0xd9d9d9",
+"0xbc80bd",
+"0xccebc5",
+"0xffed6f",
+"0xa6cee3", // second list starts here
+"0x1f78b4",
+"0xb2df8a",
+"0x33a02c",
+"0xfb9a99",
+"0xe31a1c",
+"0xfdbf6f",
+"0xff7f00",
+"0xcab2d6",
+"0x6a3d9a",
+"0xffff99",
+"0xb15928"]
 
 /**
  *
@@ -106,20 +108,20 @@ function convertXMLToString(node) {
  * @param {index} int, the index of the color to retrieve.
  *
  * @return string representation of the hexadecimal value for a color in the
- * list of k_QIIME_COLORS. If this value value is greater than the number of
- * colors available, the function will just rollover and retrieve the next
+ * list of k_COLORBREWER_COLORS. If this value value is greater than the number
+ * of colors available, the function will just rollover and retrieve the next
  * available color.
  *
- * See k_QIIME_COLORS at the top level of this module.
+ * See k_COLORBREWER_COLORS at the top level of this module.
  *
  */
 function getDiscreteColor(index){
-	var size = k_QIIME_COLORS.length;
-	if(index >= size){
-		index = index - (Math.floor(index/size)*size)
-	}
+  var size = k_COLORBREWER_COLORS.length;
+  if(index >= size){
+    index = index - (Math.floor(index/size)*size)
+  }
 
-	return k_QIIME_COLORS[index]
+  return k_COLORBREWER_COLORS[index]
 }
 
 /**
