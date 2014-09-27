a9e6430abcccff0a8c9fc877128585f454bc4f7f
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2014-09-26 21:30:24 -0600

    BUG: Fix bug with one color in continuous maps

--- emperor/support_files/emperor/js/util.js
+++ emperor/support_files/emperor/js/util.js
@@ -155,6 +155,13 @@ function getDiscreteColor(index){
 function getColorList(values, discrete, map) {
   var colors = {}, numColors = values.length-1, counter=0, interpolator;
 
+  // 1 color and continuous coloring should return the first element of the map
+  if (numColors === 0 && discrete === false){
+    console.log('punchatelas'+chroma.brewer[map][0])
+    colors[values[0]] = chroma.brewer[map][0];
+    return colors;
+  }
+
   if (discrete === false){
     map = chroma.brewer[map];
     interpolator = chroma.interpolate.bezier([map[0], map[3], map[4], map[5],
