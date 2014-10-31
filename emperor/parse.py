ace6fdbd558a0cacd9ad917922ffd29c8a9b4cb2
Greg Caporaso <gregcaporaso@gmail.com>
2014-10-31 08:27:41 -0700

    BUG: generalized to handle input as StringIO or file

--- emperor/parse.py
+++ emperor/parse.py
@@ -11,6 +11,8 @@ __maintainer__ = "Jose Antonio Navas Molina"
 __email__ = "josenavasmolina@gmail.com"
 __status__ = "Development"
 
+from StringIO import StringIO
+
 from skbio.stats.ordination import OrdinationResults
 from skbio.io import FileFormatError
 
@@ -38,6 +40,9 @@ def parse_coords(lines):
         return (pcoa_results.site_ids, pcoa_results.site, pcoa_results.eigvals,
                 pcoa_results.proportion_explained)
     except FileFormatError:
-        if type(lines) == file:
+        try:
             lines.seek(0)
+        except AttributeError:
+            # looks like we have a list of lines, not a file-like object
+            pass
         return qiime_parse_coords(lines)
