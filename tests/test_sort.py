9397166af779631f4adb16a67fb6a67b451c86b8
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2014-11-14 13:08:32 -0700

    REL: Update __version__ and __status__ strings

--- tests/test_sort.py
+++ tests/test_sort.py
@@ -6,10 +6,10 @@ __author__ = "Yoshiki Vazquez Baeza"
 __copyright__ = "Copyright 2013, The Emperor Project"
 __credits__ = ["Yoshiki Vazquez Baeza"]
 __license__ = "BSD"
-__version__ = "0.9.4-dev"
+__version__ = "0.9.5"
 __maintainer__ = "Yoshiki Vazquez Baeza"
 __email__ = "yoshiki89@gmail.com"
-__status__ = "Development"
+__status__ = "Release"
 
 from unittest import TestCase, main
 
