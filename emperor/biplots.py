fd01ffde94b1bc2aeb20a60d508e743d88d84f72
Jamie Morton <jamietmorton@gmail.com>
2015-03-23 16:59:49 -0700

    STY: PEP8 on biplots
    biplots and test_biplots

--- emperor/biplots.py
+++ emperor/biplots.py
@@ -1,22 +1,13 @@
 #!/usr/bin/env python
 # File created on 14 Apr 2013
 from __future__ import division
-
-__author__ = "Yoshiki Vazquez Baeza"
-__copyright__ = "Copyright 2013, The Emperor Project"
-__credits__ = ["Yoshiki Vazquez Baeza"]
-__license__ = "BSD"
-__version__ = "0.9.51-dev"
-__maintainer__ = "Yoshiki Vazquez Baeza"
-__email__ = "yoshiki89@gmail.com"
-__status__ = "Development"
-
 from numpy import argsort, array
 
 from emperor.util import EmperorUnsupportedComputation
 from emperor.sort import sort_taxa_table_by_pcoa_coords
 from emperor.qiime_backports.biplots import (get_taxa_prevalence,
-    get_taxa_coords, make_biplot_scores_output)
+                                             get_taxa_coords,
+                                             make_biplot_scores_output)
 
 
 def extract_taxa_data(otu_coords, otu_table, lineages, prevalence, N=0):
@@ -34,7 +25,8 @@ def extract_taxa_data(otu_coords, otu_table, lineages, prevalence, N=0):
     Outputs:
     otu_coords: N most prevalent coords
     out_otu_table: N most prevalent rows of the otu_table
-    out_otu_lineages: N most prevalent taxonomic assignments for each row of the
+    out_otu_lineages: N most prevalent taxonomic assignments
+                      for each row of the
     otu_table
     out_prevalence: first N values of prevalence
 
@@ -42,7 +34,7 @@ def extract_taxa_data(otu_coords, otu_table, lineages, prevalence, N=0):
     generic data that's not in dict forma and returns the appropriate result.
     """
     # If less than zero or greater than length of taxa, N = fix to max
-    if N<=0 or N>len(prevalence):
+    if N <= 0 or N > len(prevalence):
         N = len(prevalence)
 
     # get the first N indices to keep from all of the taxa data
@@ -56,6 +48,7 @@ def extract_taxa_data(otu_coords, otu_table, lineages, prevalence, N=0):
     out_prevalence = prevalence[indices]
 
     return out_otu_coords, out_otu_table, out_otu_lineages, out_prevalence
+
 
 def preprocess_otu_table(otu_sample_ids, otu_table, lineages,
                          coords_data, coords_headers, N=0):
@@ -84,18 +77,23 @@ def preprocess_otu_table(otu_sample_ids, otu_table, lineages,
 
     # this means there's only one or fewer rows in the contingency table
     if len(otu_table) <= 1 or len(lineages) <= 1:
-        raise EmperorUnsupportedComputation, "Biplots are not supported for "+\
-            "contingency tables with one or fewer rows"
+        raise EmperorUnsupportedComputation(
+            "Biplots are not supported for " +
+            "contingency tables with one or fewer rows")
 
     # if this element is a list take the first headers and coordinates
-    # both of these will be the master coordinates, i. e. where data is centered
+    # both of these will be the master coordinates,
+    # i. e. where data is centered
     if type(coords_data) == list and type(coords_headers) == list:
         coords_data = coords_data[0]
         coords_headers = coords_headers[0]
 
-    # re-arrange the otu table so it matches the order of the samples in the
-    # coordinates data & remove any sample that is not in the coordinates header
-    otu_sample_ids, otu_table = sort_taxa_table_by_pcoa_coords(coords_headers,
+    # re-arrange the otu table so it matches the
+    # order of the samples in the coordinates
+    # data & remove any sample that is not in the
+    # coordinates header
+    otu_sample_ids, otu_table = sort_taxa_table_by_pcoa_coords(
+        coords_headers,
         otu_table, otu_sample_ids)
 
     # retrieve the prevalence and the coords prior the filtering
@@ -105,7 +103,7 @@ def preprocess_otu_table(otu_sample_ids, otu_table, lineages,
     o_otu_coords, o_otu_table, o_otu_lineages, o_prevalence =\
         extract_taxa_data(bi_plot_coords, otu_table, lineages, prevalence, N)
 
-    lines = '\n'.join(make_biplot_scores_output({'coord': o_otu_coords,
-        'lineages': o_otu_lineages}))
+    lines = '\n'.join(make_biplot_scores_output(
+        {'coord': o_otu_coords, 'lineages': o_otu_lineages}))
 
     return o_otu_coords, o_otu_table, o_otu_lineages, o_prevalence, lines
