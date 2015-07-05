1e093769c47b4d57b7c21151233d1d15ca4b0e81
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2015-07-05 00:50:15 -0700

    ENH: Add work in progress for EmperorController

--- emperor/support_files/js/controller.js
+++ emperor/support_files/js/controller.js
@@ -13,22 +13,33 @@
  *
  */
 
-EmperorController = function(width, height, dm, divId){
-  this.width = width;
-  this.height = height;
+EmperorController = function(dm, divId){
+  this.$divId = $('#' + divId);
+  this.width = this.$divId.width();
+  this.height = this.$divId.height();
+
   this.dm = dm;
   this.sceneViews = [];
-  this.divId = divId;
+
+  // main divs where the content of the plots will be located
+  this.$plotSpaceId = $('<div id="emperor-plot-wrapper"></div>');
+  this.$plotMenu = $('<div id="emperor-menu"></div>');
+
+  this.$divId.append(this.$plotSpaceId);
+  this.$divId.append(this.$plotMenu);
 
   // set up the renderer
   this.rendererBackgroundColor = new THREE.Color();
   this.rendererBackgroundColor.setHex("0x000000");
 
   this.renderer = new THREE.WebGLRenderer( {antialias: true});
-  this.renderer.setSize(width, height);
+  this.renderer.setSize(this.width, this.height);
   this.renderer.setClearColor(this.rendererBackgroundColor);
   this.renderer.autoClear = false;
   this.renderer.sortObjects = true;
+  this.$plotSpaceId.append(this.renderer.domElement);
+
+  this.buildUI();
 
   // default decomposition view uses the full window
   this.addView();
@@ -40,7 +51,7 @@ EmperorController.prototype.addView = function(){
   }
 
   var spv = new ScenePlotView3D(this.renderer, [new DecompositionView(this.dm)],
-                                this.divId, 0, 0, 0, 0);
+                                this.$plotSpaceId.attr('id'), 0, 0, 0, 0);
   this.sceneViews.push(spv);
 
   // this will setup the appropriate sizes and widths
@@ -48,30 +59,43 @@ EmperorController.prototype.addView = function(){
 };
 
 EmperorController.prototype.resize = function(width, height){
+  // update the available space we have
   this.width = width;
   this.height = height;
 
+  // the area we have to present the plot is smaller than the total
+  var plotWidth = this.$plotSpaceId.width();
+
   if (this.sceneViews.length === 1){
-    this.sceneViews[0].resize(0, 0, this.width, this.height);
+    this.sceneViews[0].resize(0, 0, plotWidth, this.height);
   }
   else if(this.sceneViews.length === 2){
-    this.sceneViews[0].resize(0, 0, 0.5 * this.width, this.height);
-    this.sceneViews[1].resize(0.5 * this.width, 0, 0.5 * this.width, this.height);
+    this.sceneViews[0].resize(0, 0, 0.5 * plotWidth, this.height);
+    this.sceneViews[1].resize(0.5 * plotWidth, 0, 0.5 * plotWidth, this.height);
   }
   else if(this.sceneViews.length === 3){
-    this.sceneViews[0].resize(0, 0, 0.5 * this.width, 0.5 * this.height);
-    this.sceneViews[1].resize(0.5 * this.width, 0, 0.5 * this.width, 0.5 * this.height);
-    this.sceneViews[2].resize(0, 0.5 * this.height, this.width, 0.5 * this.height);
+    this.sceneViews[0].resize(0, 0, 0.5 * plotWidth, 0.5 * this.height);
+    this.sceneViews[1].resize(0.5 * plotWidth, 0, 0.5 * plotWidth, 0.5 * this.height);
+    this.sceneViews[2].resize(0, 0.5 * this.height, plotWidth, 0.5 * this.height);
   }
   else if(this.sceneViews.length === 4){
-    this.sceneViews[0].resize(0, 0, 0.5 * this.width, 0.5 * this.height);
-    this.sceneViews[1].resize(0.5 * this.width, 0, 0.5 * this.width, 0.5 * this.height);
-    this.sceneViews[2].resize(0, 0.5 * this.height, 0.5 * this.width, 0.5 * this.height);
-    this.sceneViews[3].resize(0.5 * this.width, 0.5 * this.height, 0.5 * this.width, 0.5 * this.height);
+    this.sceneViews[0].resize(0, 0, 0.5 * plotWidth, 0.5 * this.height);
+    this.sceneViews[1].resize(0.5 * plotWidth, 0, 0.5 * plotWidth, 0.5 * this.height);
+    this.sceneViews[2].resize(0, 0.5 * this.height, 0.5 * plotWidth, 0.5 * this.height);
+    this.sceneViews[3].resize(0.5 * plotWidth, 0.5 * this.height, 0.5 * plotWidth, 0.5 * this.height);
   }
   else{
     throw Error('More than four views are currently not supported');
   }
+
+  this.renderer.setSize(plotWidth, this.height);
+
+  // resize the grid according to the size of the container, since we are
+  // inside the tabs we have to account for that lost space, hence the 0.9
+  var gridWidth = this.$plotMenu.width()*0.9,
+      gridHeight = this.$plotMenu.height()*0.9;
+  $('#myGrid').width(gridWidth);
+  $('#myGrid').height(gridHeight);
 };
 
 EmperorController.prototype.render = function(){
@@ -80,4 +104,53 @@ EmperorController.prototype.render = function(){
   for (var i = 0; i < this.sceneViews.length; i++) {
     this.sceneViews[i].render();
   };
+};
+
+EmperorController.prototype.buildUI = function(){
+
+  this.$plotMenu.append('<div id="emperor-menu-tabs"></div>');
+  $('#emperor-menu-tabs').append('<ul><li><a href="#keys">Key</a></li></ul>');
+  $('#emperor-menu-tabs').append('<div id="keys" class="emperor-tab-div"></div>');
+  $('#emperor-menu-tabs').tabs({heightStyle: "fill"});
+
+  var gridWidth = this.$plotMenu.width()*0.9,
+      gridHeight = this.$plotMenu.height()*0.9;
+
+
+  $('#keys').append('<div id="myGrid"></div>');
+  $('#myGrid').width(gridWidth);
+  $('#myGrid').height(gridHeight);
+
+  var grid;
+  var columns = [
+    {id: "title", name: "", field:"color", sortable: false,
+     maxWidth:25, minWidth:25, editor: ColorEditor, formatter: ColorFormatter},
+    {id: "field1", name: "Category Name", field: "category"}
+  ];
+
+  var options = {
+    editable: true,
+    enableAddRow: false,
+    enableCellNavigation: true,
+    forceFitColumns: true,
+  };
+
+  $(function (ec) {
+    var data = [];
+    var categories = ec.dm.getUniqueValuesByCategory('DOB');
+    for (var i = 0; i < categories.length; i++) {
+      data[i] = {
+        category: categories[i],
+        color: '#00ff00'
+      };
+    }
+
+    grid = new Slick.Grid("#myGrid", data, columns, options);
+
+    // make the columns fit the available spce whenever the window resizes
+    // http://stackoverflow.com/a/29835739
+    $(window).resize(function(){
+      grid.setColumns(grid.getColumns());
+    });
+  }(this));
 };
