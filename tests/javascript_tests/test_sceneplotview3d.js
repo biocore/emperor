bb554cc3ff8ea5f4cf04bf7c2f1da713aa4cac38
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2015-12-18 18:07:11 -0800

    TST: Add minimal tests for sceneplotview3d

---
+++ tests/javascript_tests/test_sceneplotview3d.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  module('ScenePlotView3D', {
+
+    setup: function(){
+    },
+
+    teardown: function(){
+    }
+
+  });
+
+  test('Test things', function(){
+    equal(1, 1);
+  });
+});
