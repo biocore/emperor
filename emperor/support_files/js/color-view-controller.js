1fbddfc745e1c48a8ea266d30bb8a5d1193ad763
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2015-12-26 01:10:49 -0600

    MAINT/API: Move all the colors functions to CVC
    All the functions and constants that generated colors or were related to
    any of the coloring functionality, now live in ColorViewController
    (CVC), these are either class variables or class methods. All the tests
    were moved from test_util.js to test_color_view_controller.js
    
    CVC = ColorViewController.

--- emperor/support_files/js/color-view-controller.js
+++ emperor/support_files/js/color-view-controller.js
@@ -65,3 +65,112 @@ function ColorViewController(container, decompViewDict){
 }
 ColorViewController.prototype = Object.create(EmperorAttributeABC.prototype);
 ColorViewController.prototype.constructor = EmperorAttributeABC;
+
+
+/**
+ *
+ * Generate a list of colors that corresponds to all the samples in the plot
+ *
+ * @param {values} list of objects to generate a color for, usually a category
+ * in a given metadata column.
+ * @param {map} name of the color map to use, see ColorViewController.Colormaps
+ *
+ *
+ * This function will generate a list of coloring values depending on the
+ * coloring scheme that the system is currently using (discrete or continuous).
+*/
+ColorViewController.getColorList = function(values, map) {
+  var colors = {}, numColors = values.length-1, counter=0, interpolator,
+      discrete = false;
+
+  if (ColorViewController.Colormaps.indexOf(map) === -1) {
+    throw new Error("Could not find "+map+" in the available colormaps");
+  }
+
+  if (map === 'discrete-coloring' || map === 'discrete-coloring-qiime'){
+    discrete = true;
+  }
+
+  // 1 color and continuous coloring should return the first element of the map
+  if (numColors === 0 && discrete === false){
+    colors[values[0]] = chroma.brewer[map][0];
+    return colors;
+  }
+
+  if (discrete === false){
+    map = chroma.brewer[map];
+    interpolator = chroma.interpolate.bezier([map[0], map[3], map[4], map[5],
+                                              map[8]]);
+  }
+
+  for(var index in values){
+    if(discrete){
+      // get the next available color
+      colors[values[index]] = ColorViewController.getDiscreteColor(index, map);
+    }
+    else{
+      colors[values[index]] =  interpolator(counter/numColors).hex();
+      counter = counter + 1;
+    }
+  }
+
+  return colors;
+};
+
+/**
+ *
+ * Retrieve a discrete color.
+ *
+ * @param {index} int, the index of the color to retrieve.
+ * @param {map} string, name of the discrete color map to use.
+ *
+ * @return string representation of the hexadecimal value for a color in the
+ * list the QIIME colors or the ColorBrewer discrete colors. If this value
+ * value is greater than the number of colors available, the function will just
+ * rollover and retrieve the next available color.
+ *
+ * Defaults to use ColorBrewer colors if there's no map passed in.
+ *
+ */
+ColorViewController.getDiscreteColor = function(index, map){
+  if (map === undefined){
+    map = 'discrete-coloring';
+  }
+  if (_.has(ColorViewController._discreteColormaps, map) === false){
+    throw new Error("Could not find "+map+" as a discrete colormap.");
+  }
+
+  var size = ColorViewController._discreteColormaps[map].length;
+  if(index >= size){
+    index = index - (Math.floor(index/size)*size);
+  }
+
+  return ColorViewController._discreteColormaps[map][index];
+};
+
+ColorViewController.Colormaps = ['discrete-coloring-qiime',
+  'discrete-coloring', 'OrRd', 'PuBu', 'BuPu', 'Oranges', 'BuGn', 'YlOrBr',
+  'YlGn', 'Reds', 'RdPu', 'Greens', 'YlGnBu', 'Purples', 'GnBu', 'Greys',
+  'YlOrRd', 'PuRd', 'Blues', 'PuBuGn', 'Spectral', 'RdYlGn', 'RdBu', 'PiYG',
+  'PRGn', 'RdYlBu', 'BrBG', 'RdGy', 'PuOr'];
+ColorViewController.ColormapNames = ['Classic QIIME Colors',
+  'Discrete Coloring (Colorbrewer)', 'Orange-Red', 'Purple-Blue',
+  'Blue-Purple', 'Oranges', 'Blue-Green', 'Yellow-Orange-Brown',
+  'Yellow-Green', 'Reds', 'Red-Purple', 'Greens', 'Yellow-Green-Blue',
+  'Purples', 'Green-Blue', 'Greys', 'Yellow-Orange-Red', 'Purple-Red', 'Blues',
+  'Purple-Blue-Green', 'Spectral', 'Red-Yellow-Green', 'Red-Blue',
+  'Pink-Yellow-Green', 'Pink-Red-Green', 'Red-Yellow-Blue', 'Brown-Blue-Green',
+  'Red-Grey', 'Purple-Orange'];
+ColorViewController._colorbrewerDiscrete = ["#8dd3c7", "#ffffb3", "#bebada",
+  "#fb8072", "#80b1d3", "#fdb462", "#b3de69", "#fccde5", "#d9d9d9", "#bc80bd",
+  "#ccebc5", "#ffed6f", /*first list ends here*/ "#a6cee3", "#1f78b4",
+  "#b2df8a", "#33a02c", "#fb9a99", "#e31a1c", "#fdbf6f", "#ff7f00", "#cab2d6",
+  "#6a3d9a", "#ffff99", "#b15928"];
+
+// taken from the qiime/colors.py module; a total of 24 colors
+ColorViewController._qiimeDiscrete = [ "#ff0000", "#0000ff", "#f27304",
+  "#008000", "#91278d", "#ffff00", "#7cecf4", "#f49ac2", "#5da09e", "#6b440b",
+  "#808080", "#f79679", "#7da9d8", "#fcc688", "#80c99b", "#a287bf", "#fff899",
+  "#c49c6b", "#c0c0c0", "#ed008a", "#00b6ff", "#a54700", "#808000", "#008080"];
+ColorViewController._discreteColormaps = {"discrete-coloring":ColorViewController._colorbrewerDiscrete,
+                           "discrete-coloring-qiime":ColorViewController._qiimeDiscrete};
--- tests/javascript_tests/test_color_view_controller.js
+++ tests/javascript_tests/test_color_view_controller.js
@@ -61,22 +61,113 @@ $(document).ready(function() {
     teardown: function(){
       sharedDecompositionViewDict = undefined;
       $("#fooligans").remove();
-      decomp=undefined;
+      decomp = undefined;
     }
   });
 
   test("Constructor tests", function(assert) {
     var container = $('<div id="does-not-exist" style="height:11px; width:12px"></div>');
 
-    assert.ok(ColorViewController.prototype instanceof
-              EmperorAttributeABC);
+    assert.ok(ColorViewController.prototype instanceof EmperorAttributeABC);
 
     var controller = new ColorViewController(container, sharedDecompositionViewDict);
     equal(controller.title, 'Color');
 
-    var testColumn = controller.bodyGrid.getColumns()[0]
+    var testColumn = controller.bodyGrid.getColumns()[0];
     equal(testColumn.field, 'color');
   });
+
+  test("Test discrete colors are retrieved correctly", function(){
+    equal(ColorViewController.getDiscreteColor(0), "#8dd3c7", "Test color is indeed red");
+    equal(ColorViewController.getDiscreteColor(1), "#ffffb3", "Test color is indeed blue");
+    equal(ColorViewController.getDiscreteColor(2), "#bebada", "Test color is indeed orange");
+  });
+
+  test("Test discrete colors are retrieved on roll-over", function(){
+    equal(ColorViewController.getDiscreteColor(24), "#8dd3c7", "Test color is indeed red even in"+
+          " the event of a roll-over");
+    equal(ColorViewController.getDiscreteColor(25), "#ffffb3", "Test color is indeed red even in"+
+          " the event of a roll-over");
+    equal(ColorViewController.getDiscreteColor(26), "#bebada", "Test color is indeed red even in"+
+          " the event of a roll-over");
+  });
+
+  test("Test discrete colors with other maps", function(){
+    equal(ColorViewController.getDiscreteColor(0, 'discrete-coloring'), "#8dd3c7",
+          "Test color is indeed red");
+    equal(ColorViewController.getDiscreteColor(1, 'discrete-coloring-qiime'), "#0000ff",
+          "Test color is indeed blue");
+  });
+
+  test("Test discrete colors with other maps (rollover)", function(){
+    equal(ColorViewController.getDiscreteColor(24, "discrete-coloring"), "#8dd3c7",
+          "Test color is indeed red even in the event of a roll-over");
+    equal(ColorViewController.getDiscreteColor(25, "discrete-coloring-qiime"), "#0000ff",
+          "Test color is indeed red even in the event of a roll-over");
+    equal(ColorViewController.getDiscreteColor(26, "discrete-coloring-qiime"), "#f27304",
+          "Test color is indeed red even in the event of a roll-over");
+  });
+
+
+  test("Test ColorViewController.getColorList exceptions", function(){
+    var five;
+    five = [0, 1, 2, 3, 4];
+
+    throws(
+      function (){
+        var color = ColorViewController.getDiscreteColor(0, 'discrete-coloring-non-existant');
+      },
+      Error,
+      'An error is raised if the colormap does not exist'
+    );
+  });
+
+  test("Test ColorViewController.getColorList works", function(){
+    var five, ten, twenty;
+
+    five = [0, 1, 2, 3, 4];
+    ten = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    twenty = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+              19]
+
+
+    deepEqual(ColorViewController.getColorList(five, 'discrete-coloring'), { "0": "#8dd3c7", "1":
+        "#ffffb3", "2": "#bebada", "3": "#fb8072", "4": "#80b1d3" });
+
+    deepEqual(ColorViewController.getColorList(five, 'OrRd'), { "0": "#fff7ec", "1":
+        "#ffbf92", "2": "#fc8d59", "3": "#d6593a", "4": "#7f0000" });
+    deepEqual(ColorViewController.getColorList([0], 'OrRd'), {"0": "#fff7ec"});
+
+    deepEqual(ColorViewController.getColorList(ten, 'Blues'), { "0": "#f7fbff", "1":
+        "#d2e6f3", "2": "#b1d3e8", "3": "#93c3df", "4": "#78b4d9", "5":
+        "#63a7d1", "6": "#5092c3", "7": "#3c77ac", "8": "#26568f", "9":
+        "#08306b" });
+    deepEqual(ColorViewController.getColorList([0], 'Blues'), {"0": "#f7fbff"});
+
+    deepEqual(ColorViewController.getColorList(twenty, 'BrBG'), { "0": "#543005", "1":
+        "#6f4b1d", "10": "#f6e9c8", "11": "#f2e9cf", "12": "#eae7d3", "13":
+        "#dee3d3", "14": "#cedcd0", "15": "#bad3c9", "16": "#a2c7bf", "17":
+        "#85bab2", "18": "#63a9a2", "19": "#35978f", "2": "#886533", "3":
+        "#9f7d49", "4": "#b4945e", "5": "#c6a973", "6": "#d5bb87", "7":
+        "#e2cb99", "8": "#ecd9ab", "9": "#f3e4bb" });
+    deepEqual(ColorViewController.getColorList([0], 'OrRd'), {"0": "#fff7ec"});
+
+  });
+
+  test("Test ColorViewController.getColorList exceptions", function(){
+    var five;
+    five = [0, 1, 2, 3, 4];
+
+    throws(
+        function (){
+          var colors = ColorViewController.getColorList(five, false, 'Non-existant');
+        },
+      Error,
+      'An error is raised if the colormap does not exist'
+    );
+
+  });
+
 
 
 });
