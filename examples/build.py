6e7a3b01994490e1b0acc6690d745df80afc653f
Joshua Shorenstein <squirrelo@gmail.com>
2016-04-20 11:33:02 -0700

    render using given template

--- examples/build.py
+++ examples/build.py
@@ -11,7 +11,8 @@ within the examples folder.
 """
 from __future__ import division
 
-from jinja2 import Template
+from jinja2 import DictLoader
+from jinja2.environment import Environment
 from random import sample
 from sys import argv
 
@@ -21,25 +22,30 @@ import numpy as np
 def listify(a):
     return np.asarray(a, dtype='str').tolist()
 
-with open('new-emperor.html', 'w') as f, \
-        open('../emperor/support_files/templates/main-template.html') as temp:
-    template = Template(temp.read())
+# From http://stackoverflow.com/a/6196098
+pages = ['../emperor/support_files/templates/main-template.html',
+         'template.html']
+env = Environment()
+templates = dict((name, open(name, 'rb').read()) for name in pages)
+env.loader = DictLoader(templates)
+template = env.get_template('template.html')
 
-    N = 10
-    if len(argv) > 1:
-        N = int(argv[1])
+N = 10
+if len(argv) > 1:
+    N = int(argv[1])
 
-    categories = np.asarray(np.random.randint(1, 1000, N), str)
+categories = np.asarray(np.random.randint(1, 1000, N), str)
 
-    coords_ids = listify(np.arange(N))
-    coords = np.random.randn(N, 10).tolist()
-    pct_var = (1/np.exp(np.arange(10))).tolist()
+coords_ids = listify(np.arange(N))
+coords = np.random.randn(N, 10).tolist()
+pct_var = (1/np.exp(np.arange(10))).tolist()
 
-    md_headers = ['SampleID', 'DOB']
-    metadata = []
-    for _id in coords_ids:
-        metadata.append([_id, ''.join(sample(categories, 1))])
+md_headers = ['SampleID', 'DOB']
+metadata = []
+for _id in coords_ids:
+    metadata.append([_id, ''.join(sample(categories, 1))])
 
+with open('new-emperor.html', 'w') as f:
     f.write(template.render(coords_ids=coords_ids, coords=coords,
                             pct_var=pct_var, md_headers=md_headers,
                             metadata=metadata, base_url='.'))
