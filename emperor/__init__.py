47d735bcbfbd8fc467948a139261bdf5a7092d82
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2016-07-11 12:07:29 -0700

    DOC: Improve documentation

--- emperor/__init__.py
+++ emperor/__init__.py
@@ -1,3 +1,29 @@
+r"""
+Emperor 3D PCoA viewer (:mod:`emperor`)
+============================================
+
+This module provides an objects and functions to interact and visualize a set
+of coordinates using the Emperor interface user interface. The `Emperor` class,
+and the functions present here are all intended to be compatible with the
+Jupyter notebook.
+
+.. currentmodule:: emperor
+
+Classes
+-------
+.. autosummary::
+    :toctree: generated/
+
+    Emperor
+
+Functions
+---------
+.. autosummary::
+    :toctree: generated/
+
+    nbinstall
+    scatterplot
+"""
 # ----------------------------------------------------------------------------
 # Copyright (c) 2013--, emperor development team.
 #
@@ -8,7 +34,8 @@
 __version__ = "0.9.51-dev"  # noqa
 
 from emperor.core import Emperor
-from emperor.pandas import scatterplot
+from emperor._pandas import scatterplot
+from emperor.util import nbinstall
 
 __all__ = ['Emperor', 'scatterplot', 'biplots', 'format', 'filter', 'parse',
-           'sort', 'util']
+           'sort', 'util', 'nbinstall']
