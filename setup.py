92f8c87bb9dbc04ecb5620f65301aa4b6acaff07
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2016-11-01 13:31:34 -0700

    REL: Bump Versions

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@
 from distutils.core import setup
 from glob import glob
 
-__version__ = "0.9.51-dev"
+__version__ = "0.9.6"
 __maintainer__ = "Emperor development team"
 __email__ = "yoshiki89@gmail.com"
 
