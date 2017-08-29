af716a3123f84d5544ef0c40512f6635ee6949cf
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2017-08-29 07:38:47 -0700

    REL: Release beta 9 (#603)
    * REL: Release beta 9
    
    * MAINT: Remove forgotten console.log

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import sys
 from distutils.core import setup
 from glob import glob
 
-__version__ = "1.0.0beta8-dev"
+__version__ = "1.0.0beta9"
 __maintainer__ = "Emperor development team"
 __email__ = "yoshiki89@gmail.com"
 
