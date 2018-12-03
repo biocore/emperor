b537a1a282fed0ca7fa8df6c423609b5b4ddb548
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2018-12-03 12:30:49 -0800

    Add descriptor for Python 3.4

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ classes = """
     Topic :: Software Development :: User Interfaces
     Programming Language :: Python
     Programming Language :: Python :: 2.7
+    Programming Language :: Python :: 3.4
     Programming Language :: Python :: 3.5
     Programming Language :: Python :: 3.6
     Programming Language :: Python :: 3.7
