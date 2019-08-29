9020550046d76967d3a892b5e49f5df9fb3e358d
Xuhan Yang <ireneyxh@gmail.com>
2019-08-29 10:38:37 -0700

    08/29 update

--- emperor/support_files/vendor/js/three.js-plugins/SelectionBox.js
+++ emperor/support_files/vendor/js/three.js-plugins/SelectionBox.js
@@ -68,64 +68,50 @@ define(['three'], function(THREE){
   		this.camera.updateProjectionMatrix();
   		this.camera.updateMatrixWorld();
 
-      var z_value = ( (this.camera.near + this.camera.far)/(this.camera.near - this.camera.far) ) * this.camera.zoom;
+      var zoom = this.camera.zoom;
+      // var z_value = ( (this.camera.near + this.camera.far)/(this.camera.near - this.camera.far) ) * this.camera.zoom;
 
   		tmpPoint.copy( startPoint );
   		tmpPoint.x = Math.min( startPoint.x, endPoint.x );
   		tmpPoint.y = Math.max( startPoint.y, endPoint.y );
-      tmpPoint.z = z_value;
   		endPoint.x = Math.max( startPoint.x, endPoint.x );
   		endPoint.y = Math.min( startPoint.y, endPoint.y );
-      endPoint.z = z_value;
 
   		vecNear.copy( this.camera.position );
 
   		vecTopLeft.copy( tmpPoint );
-  		vecTopRight.set( endPoint.x, tmpPoint.y, z_value );
+      vecTopLeft.z = 0;
+  		vecTopRight.set( endPoint.x, tmpPoint.y, 0);
   		vecDownRight.copy( endPoint );
-  		vecDownLeft.set( tmpPoint.x, endPoint.y, z_value );
+      vecDownRight.z = 0;
+  		vecDownLeft.set( tmpPoint.x, endPoint.y, 0);
 
-      console.log(this.camera.zoom);
+      vectemp1.copy( vecTopLeft );
+      vectemp1.z = this.camera.far;
+      vectemp2.copy( vecTopRight );
+      vectemp2.z = this.camera.far;
+      vectemp3.copy( vecDownRight );
+      vectemp3.z = this.camera.far;
 
-  		vecTopLeft.unproject( this.camera );
+      vecTopLeft.unproject( this.camera );
   		vecTopRight.unproject( this.camera );
   		vecDownRight.unproject( this.camera );
   		vecDownLeft.unproject( this.camera );
+      vectemp1.unproject( this.camera );
+      vectemp2.unproject( this.camera );
+      vectemp3.unproject( this.camera );
 
-  		vectemp1.copy( vecTopLeft ).sub( vecNear );
-  		vectemp2.copy( vecTopRight ).sub( vecNear );
-  		vectemp3.copy( vecDownRight ).sub( vecNear );
-  		vectemp1.normalize();
-  		vectemp2.normalize();
-  		vectemp3.normalize();
-
-  		vectemp1.multiplyScalar( this.deep);
-  		vectemp2.multiplyScalar( this.deep);
-  		vectemp3.multiplyScalar( this.deep);
-  		vectemp1.add( vecNear);
-  		vectemp2.add( vecNear);
-  		vectemp3.add( vecNear);
-
-
+  		vectemp1.add( vecNear );
+  		vectemp2.add( vecNear );
+  		vectemp3.add( vecNear );
 
   		var planes = frustum.planes;
-
-      // planes[ 0 ].setFromCoplanarPoints( vecNear, vecTopLeft, vecTopRight );
-  		// planes[ 1 ].setFromCoplanarPoints( vecNear, vecTopRight, vecDownRight );
-  		// planes[ 2 ].setFromCoplanarPoints( vecDownRight, vecDownLeft, vecNear );
-  		// planes[ 3 ].setFromCoplanarPoints( vecDownLeft, vecTopLeft, vecNear );
-  		// planes[ 4 ].setFromCoplanarPoints( vecTopRight, vecDownRight, vecDownLeft );
-  		// planes[ 5 ].setFromCoplanarPoints( vectemp3, vectemp2, vectemp1 );
 
   		planes[ 0 ].setFromCoplanarPoints( vectemp1, vecTopLeft, vecTopRight );
   		planes[ 1 ].setFromCoplanarPoints( vectemp2, vecTopRight, vecDownRight );
   		planes[ 2 ].setFromCoplanarPoints( vectemp3, vecDownLeft, vecDownRight );
   		planes[ 3 ].setFromCoplanarPoints( vecDownLeft, vecTopLeft, vectemp1 );
   		planes[ 4 ].setFromCoplanarPoints( vecTopRight, vecDownRight, vecDownLeft );
-      console.log('the culprit?');
-      console.log(vectemp3);
-      console.log(vectemp2);
-      console.log(vectemp1);
   		planes[ 5 ].setFromCoplanarPoints( vectemp3, vectemp2, vectemp1 );
   		planes[ 5 ].normal.multiplyScalar( - 1 );
 
